091d21611039bd5a1a8ee662a1f28c04af3aa458
unknown <rislammb@gmail.com>
2019-11-06 12:54:52 +0600

    Add build folder

---
+++ build/precache-manifest.c279bced88d04dc7067184a5519b7d37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f68cb43e9c831df18356a0e9c5fb2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fdda6314a615fa2b0a5",
+    "url": "/static/css/2.7d36abf2.chunk.css"
+  },
+  {
+    "revision": "c517ade8b18992e888fd",
+    "url": "/static/css/main.b590ebb1.chunk.css"
+  },
+  {
+    "revision": "3fdda6314a615fa2b0a5",
+    "url": "/static/js/2.d3280aaf.chunk.js"
+  },
+  {
+    "revision": "c517ade8b18992e888fd",
+    "url": "/static/js/main.a898ba8f.chunk.js"
+  },
+  {
+    "revision": "ddcad31ae4d0624a35ff",
+    "url": "/static/js/runtime-main.e248060b.js"
+  }
+]);
